ba8dbadd9bd97e886f1a5607450ed071cd870c45
Christopher Jauregui <a22490157@itmexicali.edu.mx>
2025-05-01 23:58:43 +0000

    Parte2

---
+++ src/config.py
@@ -0,0 +1,9 @@
+class DevelopmentConfig(): 
+    DEBUG = True 
+    SECRET_KEY = "$Jauregui$"
+    MYSQL_HOST = "localhost" 
+    MYSQL_USER = "root" 
+    MYSQL_PASSWORD = "Jauregui" 
+    MYSQL_DB = "store" 
+config = {"development": DevelopmentConfig}
+
